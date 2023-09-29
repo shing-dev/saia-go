78be03f342289595af8b4debcfaf28c21522d4b6
k-yomo <kanji.yy@gmail.com>
2023-09-29 17:53:07 +0900

    Add error details

--- person.go
+++ person.go
@@ -1,6 +1,9 @@
 package saia
 
-import "time"
+import (
+	"strings"
+	"time"
+)
 
 type Gender string
 
@@ -44,9 +47,67 @@ type TaskSet struct {
 	SubTasks     []*SubTask `json:"sub_tasks"`
 }
 
+func (t *TaskSet) IsFailed() bool {
+	return !t.IsSuccessful && t.IsReady
+}
+
+type SubTaskName string
+
+const (
+	SubTaskNameFrontSkeletonProcessing    SubTaskName = "front_skeleton_processing"
+	SubTaskNameSideSkeletonProcessing     SubTaskName = "side_skeleton_processing"
+	SubTaskNameFrontProcessing            SubTaskName = "front_processing"
+	SubTaskNameSideProcessing             SubTaskName = "side_processing"
+	SubTaskNameMeasurementModelProcessing SubTaskName = "measurement_model_processing"
+)
+
+type SubTaskErrorCode int
+
+// We use simple iota instead of using the application code provided by 3dlook (https://saia.3dlook.me/docs/#task-errors)
+// because they are incomplete (e.g. "Front photo in the side" is not defined)
+const (
+	SubTaskErrorCodeUnknown SubTaskErrorCode = iota
+	SubTaskErrorCodeWrongPose
+	SubTaskErrorCodeHumanBodyNotDetected
+	SubTaskErrorCodeObjectIsNotHuman
+	SubTaskErrorCodeSidePhotoInTheFront
+	SubTaskErrorCodeFrontPhotoInTheSide
+	SubTaskErrorCodeBodyIsNotFull
+)
+
 type SubTask struct {
-	Name    string `json:"name"`
-	Status  string `json:"status"`
-	TaskID  string `json:"task_id"`
-	Message string `json:"message"`
+	Name    SubTaskName `json:"name"`
+	Status  TaskStatus  `json:"status"`
+	TaskID  string      `json:"task_id"`
+	Message string      `json:"message"`
+}
+
+// ErrorCode is a workaround method to extract the error code
+// It returns unknown when the sub task is not failed
+// TODO: Ask 3dlook to return application error code which is more valid than parsing error message
+func (s *SubTask) ErrorCode() SubTaskErrorCode {
+	if !s.IsFailed() {
+		return SubTaskErrorCodeUnknown
+	}
+
+	switch {
+	case strings.HasPrefix(s.Message, "The pose is wrong"):
+		return SubTaskErrorCodeWrongPose
+	case s.Message == "Can't detect the human body":
+		return SubTaskErrorCodeHumanBodyNotDetected
+	case s.Message == "The detected object is not human":
+		return SubTaskErrorCodeObjectIsNotHuman
+	case s.Message == "Side photo in the front":
+		return SubTaskErrorCodeSidePhotoInTheFront
+	case s.Message == "Front photo in the side":
+		return SubTaskErrorCodeFrontPhotoInTheSide
+	case s.Message == "The body is not full":
+		return SubTaskErrorCodeBodyIsNotFull
+	default:
+		return SubTaskErrorCodeUnknown
+	}
+}
+
+func (s *SubTask) IsFailed() bool {
+	return s.Status == TaskStatusFailure
 }
